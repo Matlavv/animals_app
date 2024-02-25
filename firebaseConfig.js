6ddfab6ca1fb76354b3f1cfba15a71e1c290d0e1
matlavv <mathis.laversin92@gmail.com>
2024-02-25 12:29:38 +0100

    Securing API key with .env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,19 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants from "expo-constants";
 import { initializeApp } from "firebase/app";
 import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+const expoConfig = Constants.expoConfig || {};
+const { extra } = expoConfig;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCS048UK1tiUPxyrq0BCRcNJSxgmVfHSN8",
-  authDomain: "animalzz-35e64.firebaseapp.com",
-  projectId: "animalzz-35e64",
-  storageBucket: "animalzz-35e64.appspot.com",
-  messagingSenderId: "197197222649",
-  appId: "1:197197222649:web:e3c103fda9068e34c00828",
-  measurementId: "G-DGDQVHHZGW",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
